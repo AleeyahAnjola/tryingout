f5093e22dbf274db1b146df01d9f7d491169d0ad
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-08-09 11:39:09 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/TryingoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryingoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryingoutApplication.class, args);
+	}
+
+}
