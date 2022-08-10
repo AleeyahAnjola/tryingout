fdaae66734c2ba0b986aaa2ed134b999818a20d3
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-08-10 08:17:50 +0100

    Second commit - Dev Tools added

--- src/main/java/com/example/demo/TryingoutApplication.java
+++ src/main/java/com/example/demo/TryingoutApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TryingoutApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TryingoutApplication.class, args);
 	}
